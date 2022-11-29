a556e1639c3da81b9edaf99b7b0c1c935782328f
JonaMasterFull <jonytello76@gmail.com>
2022-11-29 16:05:47 -0600

    Cambios 2

--- models/createdOrder.js
+++ models/createdOrder.js
@@ -4,6 +4,12 @@ class CreatedOrder {
     async ViewStatusUpdate(status, data){
         if (status === 'processing'){
             console.log('Si es Processing');
+
+            const datos = data.map((item) => {
+                item.id
+            })
+
+            console.log(datos);
         }
         else {
             console.log('No es Processing');
