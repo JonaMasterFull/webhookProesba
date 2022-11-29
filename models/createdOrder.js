d7ec8ffecb8c8b3a68d7803d86b63c0d7e46dfc1
JonaMasterFull <jonytello76@gmail.com>
2022-11-29 17:47:51 -0600

    Cambios 4

--- models/createdOrder.js
+++ models/createdOrder.js
@@ -1,13 +1,38 @@
 
 class CreatedOrder {
+    object= {}
+    
     
     async ViewStatusUpdate(status, data){
         if (status === 'processing'){
             console.log('Si es Processing');
 
             const customerOrderNumber = data.id
+            const notes = data.customer_note
+            const shipToInfo = {
+                name1: data.shipping.first_name,
+                name2: data.shipping.last_name,
+                companyName: data.shipping.company,
+                addressLine1: data.shipping.address_1,
+                addressLine2: data.shipping.address_2,
+                city:  data.shipping.city,
+                state: data.shipping.state,
+                postalCode: data.shipping.postcode,
+                countryCode: data.shipping.country,
+                phoneNumber: data.billing.phone  
+            }
+
+            const lines = data.line_items.map((items, index)=> {
+                return {
+                    'customerLineNumber': index + 1,
+                    'ingramPartNumber': items.sku,
+                    'quantity': items.quantity
+                }
+            });
+
+            console.log(customerOrderNumber, notes, shipToInfo,lines);
                
-            console.log(customerOrderNumber);
+            
         }
         else {
             console.log('No es Processing');
