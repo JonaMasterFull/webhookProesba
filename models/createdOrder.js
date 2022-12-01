fca5b8d62b689c47218f28b49ae33fdbb06fec91
JonaMasterFull <jonytello76@gmail.com>
2022-12-01 15:46:33 -0600

    Cambios 5

--- models/createdOrder.js
+++ models/createdOrder.js
@@ -1,14 +1,19 @@
+const axios = require('axios');
+
 
 class CreatedOrder {
-    object= {}
-    
     
     async ViewStatusUpdate(status, data){
+
+
         if (status === 'processing'){
             console.log('Si es Processing');
 
-            const customerOrderNumber = data.id
+            const customerOrderNumber = `RSF_${data.id}`
             const notes = data.customer_note
+
+            const estado = await this.EstadosUpdate(data.shipping.state);
+
             const shipToInfo = {
                 name1: data.shipping.first_name,
                 name2: data.shipping.last_name,
@@ -16,7 +21,7 @@ class CreatedOrder {
                 addressLine1: data.shipping.address_1,
                 addressLine2: data.shipping.address_2,
                 city:  data.shipping.city,
-                state: data.shipping.state,
+                state: estado,
                 postalCode: data.shipping.postcode,
                 countryCode: data.shipping.country,
                 phoneNumber: data.billing.phone  
@@ -38,6 +43,49 @@ class CreatedOrder {
             console.log('No es Processing');
         }
     }
+
+    async EstadosUpdate(Estado){
+
+        let state = Estado.toUpperCase()
+
+        switch (state) {
+                case 'AGU': state = "AG"; break;
+                case 'BCN': state = "BN"; break;
+                case 'BCS': state = "BS"; break;
+                case 'CAM': state = "CA"; break;
+                case 'CDMX': state = "DF"; break;
+                case 'CHS': state = "CI"; break;
+                case 'CHS': state = "CH"; break;
+                case 'COA': state = "CZ"; break;
+                case 'COL': state = "CL"; break;
+                case 'DGO': state = "DU"; break;
+                case 'EM': state = "ME"; break;
+                case 'GRO': state = "GA"; break;
+                case 'GRO': state = "GE"; break;
+                case 'HID': state = "HI"; break;
+                case 'JAL': state = "JA"; break;
+                case 'MICH': state = "MI"; break;
+                case 'MOR': state = "MO"; break;
+                case 'NAY': state = "NA"; break;
+                case 'NL': state = "NL"; break;
+                case 'OAX': state = "OA"; break;
+                case 'PUE': state = "PU"; break;
+                case 'QRO': state = "QA"; break;
+                case 'QR': state = "QR"; break;
+                case 'SLP': state = "SA"; break;
+                case 'SIN': state = "SI"; break;
+                case 'SON': state = "SO"; break;
+                case 'TAB': state = "TB"; break;
+                case 'TLA': state = "TM"; break;
+                case 'TLA': state = "TL"; break;
+                case 'VER': state = "VE"; break;
+                case 'YUC': state = "YU"; break;
+                case 'ZAC': state = "ZA"; break;
+                default: state = "15"
+            }
+            return state;
+    }
+    
 }
 
 module.exports = CreatedOrder;
